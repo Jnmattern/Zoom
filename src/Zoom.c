c63e48a51fda5354b1b87e566e11cc9cfb7d4d34
Jnmattern <jnmattern@gmail.com>
2014-02-06 15:54:00 +0100

    Added a graphics_draw_thick_line function

--- src/Zoom.c
+++ src/Zoom.c
@@ -8,10 +8,89 @@ static BitmapLayer *maskLayer;
 static GPoint center;
 static struct tm now;
 
+
+static void graphics_draw_thick_line(GContext *ctx, const GPoint p0, const GPoint p1, const int thick) {
+	bool steep;
+	int x0, y0, x1, y1, iTmp, deltax, deltay, error, ystep, x, y, xx0, yy0, xx1, yy1, thickOver2, xt;
+
+	x0 = p0.x; y0 = p0.y;
+	x1 = p1.x; y1 = p1.y;
+	
+	steep = abs(y1 - y0) > abs(x1 - x0);
+
+	if (steep) {
+		// swap(x0, y0)
+		iTmp = x0;
+		x0 = y0;
+		y0 = iTmp;
+
+		// swap(x1, y1)
+		iTmp = x1;
+		x1 = y1;
+		y1 = iTmp;
+	}
+
+	if (x0 > x1) {
+		// swap(x0, x1)
+		iTmp = x0;
+		x0 = x1;
+		x1 = iTmp;
+
+		// swap(y0, y1)
+		iTmp = y0;
+		y0 = y1;
+		y1 = iTmp;
+	}
+
+	deltax = x1 - x0;
+	deltay = abs(y1 - y0);
+	error = deltax / 2;
+	ystep = 0;
+	y = y0;
+
+	if (y0 < y1) {
+		ystep = 1;
+	} else {
+		ystep = -1;
+	}
+
+	thickOver2 = thick/2;
+	xt = x1-thick;
+	for (x = x0; x <= x1; x++) {
+		xx0 = x-thickOver2;
+		yy0 = y-thickOver2;
+		xx1 = xx0 + thick - 1;
+		yy1 = yy0 + thick - 1;
+		if (steep) {
+			//graphics_draw_rect(ctx, GRect(y-thickOver2, x-thickOver2, thick, thick));
+			graphics_draw_line(ctx, GPoint(yy0, xx0), GPoint(yy1, xx0));
+			graphics_draw_line(ctx, GPoint(yy0, xx0), GPoint(yy0, xx1));
+			if (x >= xt) {
+				graphics_draw_line(ctx, GPoint(yy1, xx0), GPoint(yy1, xx1));
+				graphics_draw_line(ctx, GPoint(yy0, xx1), GPoint(yy1, xx1));
+			}
+		} else {
+			//graphics_draw_rect(ctx, GRect(x-thickOver2, y-thickOver2, thick, thick));
+			graphics_draw_line(ctx, GPoint(xx0, yy0), GPoint(xx0, yy1));
+			graphics_draw_line(ctx, GPoint(xx0, yy0), GPoint(xx1, yy0));
+			if (x >= xt) {
+				graphics_draw_line(ctx, GPoint(xx1, yy0), GPoint(xx1, yy1));
+				graphics_draw_line(ctx, GPoint(xx0, yy1), GPoint(xx1, yy1));
+			}
+		}
+		
+		error = error - deltay;
+		if (error < 0) {
+			y = y + ystep;
+			error = error + deltax;
+		}
+	}
+}
+
 static void updateLayer(Layer *layer, GContext *ctx) {
-	int angle, i, j;
+	int angle;
 	int cos, sin;
-	GPoint a, p1, p2;
+	GPoint a;
 	static GRect r = { { 0, 0}, { 144, 168 } };
 	
 	angle = TRIG_MAX_ANGLE * (60*(now.tm_hour%12) + now.tm_min) / 720;
@@ -26,29 +105,15 @@ static void updateLayer(Layer *layer, GContext *ctx) {
 	r.size = bgBitmap->bounds.size;
 
 	graphics_draw_bitmap_in_rect(ctx, bgBitmap, r);
-	
-	graphics_context_set_stroke_color(ctx, GColorBlack);
-	
+		
 	a.x = 72 + 72*sin/TRIG_MAX_RATIO;
 	a.y = 84 - 72*cos/TRIG_MAX_RATIO;
 
-	for (j=-1; j<2; j++) {
-		for (i=-1; i<2; i++) {
-			p1 = center;
-			p2 = a;
-			p1.x += i;
-			p2.x += i;
-			p1.y += j;
-			p2.y += j;
-			
-			graphics_draw_line(ctx, p1, p2);
-		}
-	}
+	graphics_context_set_stroke_color(ctx, GColorBlack);
+	graphics_draw_thick_line(ctx, a, center, 5);
 
 	graphics_context_set_stroke_color(ctx, GColorWhite);
-	p1 = center;
-	p2 = a;
-	graphics_draw_line(ctx, p1, p2);
+	graphics_draw_line(ctx, a, center);
 }
 
 static void handleTick(struct tm *t, TimeUnits units_changed) {
